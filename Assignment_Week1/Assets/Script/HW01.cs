b3f2e27861925be79308ea7683fdd85c2d1ffaa6
unknown <siwaporn.hoys@bumail.net>
2021-01-29 16:36:29 +0700

    Send Assignment Week1

---
+++ Assignment_Week1/Assets/Script/HW01.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HW01 : MonoBehaviour
+{
+    public InputField ad;
+    public Text sa;
+    string info;
+
+    public void node()
+    {
+        if (info == " Aston Martin One ") //InputField 1
+        {
+            found();
+        }
+        else if (info == " Kawasaki ") //InputField 2
+        {
+            found();
+        }
+        else if (info == " Lamborghini ") //InputField 3
+        {
+            found();
+        }
+        else if (info == " Mercedes - Benz ") //InputField 4
+        {
+            found();
+        }
+        else if (info == " Porsche ") //InputField 5
+        {
+            found();
+        }
+        else 
+        {
+            sa.text = "[ " + info + " ]" + " is not found.";
+        }
+    }//node
+
+    void Update()
+    {
+        info = ad.text;
+
+    }// Update
+
+    void found()
+    {
+        ad.text = "[ "+ info + " ]" + " is found.";
+    }//found
+
+    public void ClearForm()
+    {
+        ad.text = "";
+    }//ClearForm
+
+    public void Cleartext()
+    {
+        sa.text = "";
+    }//Cleartext
+
+}//hw01
